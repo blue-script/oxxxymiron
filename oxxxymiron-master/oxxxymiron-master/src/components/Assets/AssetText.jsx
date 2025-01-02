d9c89be488d19be88bf1e820d8a3fe91f8565c54
blue-script <alexm92@inbox.ru>
2025-01-02 17:10:32 +0500

    fix: remove old files

--- oxxxymiron-master/oxxxymiron-master/src/components/Assets/AssetText.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AssetText = ({ value }) => (
-  <span
-    dangerouslySetInnerHTML={{ __html: value.replaceAll("\n\n", "<br />") }}
-  />
-);
-
-export default AssetText;
